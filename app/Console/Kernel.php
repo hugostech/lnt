2e5c2b86b6ee3551475f68f1a3f8832cf7d282e1
hugo <hugo.wang@grabone.co.nz>
2018-11-06 17:03:43 +1300

    scheduling tash

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('sync:push --force')->dailyAt('23:30');
     }
 
     /**
