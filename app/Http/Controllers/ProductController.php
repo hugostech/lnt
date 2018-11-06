6e98c6e5ad06107154be17c5840fd1d839dbc218
hugo <hugo.wang@grabone.co.nz>
2018-11-07 10:27:57 +1300

    add auth middleware for productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * list all trace product
      */
