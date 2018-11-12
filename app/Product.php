5ae1ed9d3236bc31786e9257a2fda702b57708d7
hankunwang <hugowangchn@gmail.com>
2018-11-12 22:05:53 +1300

    stop update price temp

--- app/Product.php
+++ app/Product.php
@@ -107,7 +107,7 @@ class Product extends Model
                 $bestPrice = $this->bottom_price*1.15*1.1;
             }
             $this->special = $bestPrice;
-            $this->save();
+            return $this->save();
             return $this->updatePrice($bestPrice);
         }else{
             return false;
